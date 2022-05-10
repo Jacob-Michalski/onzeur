30f36feb2ff60077a8eb9c4213ae6d77ea3913a8
Jeremie Opigez <jeremie.opigez@gmail.com>
2022-05-11 00:25:28 +0200

    Player vlc for rtsp flux

--- app/src/main/java/fr/uavignon/ceri/onzeur/MainActivity.kt
+++ app/src/main/java/fr/uavignon/ceri/onzeur/MainActivity.kt
@@ -2,24 +2,29 @@ package fr.uavignon.ceri.onzeur
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.content.Context
-import android.content.ContextWrapper
+import android.content.Intent
 import android.content.pm.PackageManager
-import android.media.MediaPlayer
 import android.net.Uri
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Environment
+import android.speech.RecognitionListener
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
 import android.view.MotionEvent
 import android.widget.ImageButton
-import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
-import com.google.android.material.drawable.DrawableUtils
+import org.videolan.libvlc.LibVLC
+import org.videolan.libvlc.Media
+import org.videolan.libvlc.MediaPlayer
 import java.io.File
+import java.lang.Exception
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), RecognitionListener {
+
 
     @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -37,13 +42,20 @@ class MainActivity : AppCompatActivity() {
 
         checkPerms()
 
-        var recorder: Recorder? = null
+        var speechRecognizer = SpeechRecognizer.createSpeechRecognizer(this);
+        var speechRecognizerIntent: Intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.FRENCH)
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.US)
+        speechRecognizerIntent.putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true)
+        speechRecognizer.setRecognitionListener(this)
 
         var path = getFilePath()
 
 
         topSongsRecyclerView.adapter = adapterTop
         lastSongsRecyclerView.adapter = adapterLast
+
         val buttonMic = findViewById<ImageButton>(R.id.microphoneButton)
         buttonMic.setOnTouchListener { _, motionEvent ->
             when(motionEvent.action){
@@ -55,36 +67,49 @@ class MainActivity : AppCompatActivity() {
                         val permissions = arrayOf(android.Manifest.permission.RECORD_AUDIO, android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE)
                         ActivityCompat.requestPermissions(this, permissions,0)
                     } else {
-                        recorder = Recorder(path)
+                        speechRecognizer.startListening(speechRecognizerIntent)
+                        //recorder = Recorder(path)
                         buttonMic.setBackgroundResource(R.drawable.button_shape_pressed);
-                        Toast.makeText(this, recorder!!.startRecording(), Toast.LENGTH_SHORT).show()
+                        //Toast.makeText(this, recorder!!.startRecording(), Toast.LENGTH_SHORT).show()
                     }
                 }
                 MotionEvent.ACTION_UP -> {
-                    buttonMic.setBackgroundResource(R.drawable.button_shape);
-                    Toast.makeText(this,recorder!!.stopRecording(), Toast.LENGTH_SHORT).show()
+                    buttonMic.setBackgroundResource(R.drawable.button_shape)
+                    speechRecognizer.stopListening()
+
+                    //Toast.makeText(this,recorder!!.stopRecording(), Toast.LENGTH_SHORT).show()
                 }
             }
             true;
         }
 
-
         val buttonPlay = findViewById<ImageButton>(R.id.playButton)
-        var mediaPlayer: MediaPlayer? = null
+        var libVLC : LibVLC? = null
+        libVLC = LibVLC(this, ArrayList<String>().apply {
+            add("--no-drop-late-frames")
+            add("--no-skip-frames")
+            add("--rtsp-tcp")
+            add("-vvv")
+        })
+        var mediaPlayer = MediaPlayer(libVLC)
+
         buttonPlay.setOnClickListener{
-            mediaPlayer = MediaPlayer.create(this, Uri.fromFile(File(getFilePath())))
-            mediaPlayer?.start()
+            Media(libVLC, Uri.parse("rtsp://192.168.5.66:8080/test")).apply {
+                mediaPlayer.media = this
+            }.release()
+            mediaPlayer.play()
         }
     }
-
 
     private fun checkPerms(){
         if (ContextCompat.checkSelfPermission(this,Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED
             && ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
             && ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+            && ContextCompat.checkSelfPermission(this, Manifest.permission.INTERNET) != PackageManager.PERMISSION_GRANTED
             && ContextCompat.checkSelfPermission(this, Manifest.permission.MANAGE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             val permissions = arrayOf(
                 android.Manifest.permission.RECORD_AUDIO,
+                android.Manifest.permission.INTERNET,
                 android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
                 android.Manifest.permission.READ_EXTERNAL_STORAGE,
                 android.Manifest.permission.MANAGE_EXTERNAL_STORAGE
@@ -97,5 +122,35 @@ class MainActivity : AppCompatActivity() {
         var directory = this.baseContext.getExternalFilesDir(Environment.DIRECTORY_MUSIC);
         var file = File(directory, "file.3gpp")
         return file.path
+    }
+
+    override fun onReadyForSpeech(p0: Bundle?) {
+    }
+
+    override fun onBeginningOfSpeech() {
+    }
+
+    override fun onRmsChanged(p0: Float) {
+    }
+
+    override fun onBufferReceived(p0: ByteArray?) {
+    }
+
+    override fun onEndOfSpeech() {
+    }
+
+    override fun onError(p0: Int) {
+    }
+
+    override fun onResults(p0: Bundle?) {
+        val results: ArrayList<String> =
+            p0?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION) as ArrayList<String>
+        println(results)
+    }
+
+        override fun onPartialResults(p0: Bundle?) {
+    }
+
+    override fun onEvent(p0: Int, p1: Bundle?) {
     }
 }
