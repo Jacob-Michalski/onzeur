cf4a453bbf6c438e18b8894e5780b2ef370fd010
Jeremie Opigez <jeremie.opigez@gmail.com>
2022-05-11 01:08:18 +0200

    begin api implement

--- app/src/main/java/fr/uavignon/ceri/onzeur/ApiSDP.kt
+++ app/src/main/java/fr/uavignon/ceri/onzeur/ApiSDP.kt
@@ -1,9 +1,117 @@
 package fr.uavignon.ceri.onzeur
 
+import okhttp3.*
+import okio.IOException
+
+
 class ApiSDP {
-    val _IP: String = ""
+    val _ENDPOINT: String = "http://192.168.5.66/"
+    private val client = OkHttpClient()
+    val ref: String? = null
 
+    public fun playSong(artiste: String, titre: String) {
+        val request = Request.Builder()
+            .url(_ENDPOINT)
+            .url("play/")
+            .url("$artiste/")
+            .url(titre)
+            .build()
 
-    val index: String? = null
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                    for ((name, value) in response.headers) {
+                        println("$name: $value")
+                    }
+
+                    println(response.body!!.string())
+                }
+            }
+        })
+    }
+
+    public fun pauseSong() {
+        val request = Request.Builder()
+            .url(_ENDPOINT)
+            .url("pause/")
+            .url(ref!!)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                    for ((name, value) in response.headers) {
+                        println("$name: $value")
+                    }
+
+                    println(response.body!!.string())
+                }
+            }
+        })
+    }
+
+    public fun resumeSong() {
+        val request = Request.Builder()
+            .url(_ENDPOINT)
+            .url("resume/")
+            .url(ref!!)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                    for ((name, value) in response.headers) {
+                        println("$name: $value")
+                    }
+
+                    println(response.body!!.string())
+                }
+            }
+        })
+    }
+
+    public fun stopSong() {
+        val request = Request.Builder()
+            .url(_ENDPOINT)
+            .url("stop/")
+            .url(ref!!)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                response.use {
+                    if (!response.isSuccessful) throw IOException("Unexpected code $response")
+
+                    for ((name, value) in response.headers) {
+                        println("$name: $value")
+                    }
+
+                    println(response.body!!.string())
+                }
+            }
+        })
+    }
 
 }
