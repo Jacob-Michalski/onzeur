7025a451d396f03ddeae7f060454b7678ce3903f
Jeremie Opigez <jeremie.opigez@gmail.com>
2022-05-10 18:09:34 +0200

    added listener onTouch for mic button

--- app/src/main/java/fr/uavignon/ceri/onzeur/MainActivity.kt
+++ app/src/main/java/fr/uavignon/ceri/onzeur/MainActivity.kt
@@ -2,8 +2,10 @@ package fr.uavignon.ceri.onzeur
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.app.ActionBar
+import android.view.MotionEvent
+import android.widget.ImageButton
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.drawable.DrawableUtils
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,6 +23,18 @@ class MainActivity : AppCompatActivity() {
 
         topSongsRecyclerView.adapter = adapterTop
         lastSongsRecyclerView.adapter = adapterLast
+        val buttonMic = findViewById<ImageButton>(R.id.microphoneButton)
+        buttonMic.setOnTouchListener { _, motionEvent ->
+            when(motionEvent.action){
+                MotionEvent.ACTION_DOWN -> {
+                    buttonMic.setBackgroundResource(R.drawable.button_shape_pressed);
+                }
+                MotionEvent.ACTION_UP -> {
+                    buttonMic.setBackgroundResource(R.drawable.button_shape);
+                }
+            }
+            true;
+        }
 
     }
 }
